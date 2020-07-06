2557e29eea1d29c9b44940e27e2f8e591913757d
Akhil7669 <akhilgolla569@gmail.com>
2020-07-06 15:58:38 -0300

    my-web-app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './auth-page-structure/signin/signin.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SignupComponent } from './auth-page-structure/signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent }
+  //{ path: 'second-component', component: SecondComponent },
+  //{ path: '',   redirectTo: '/first-component', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { SigninComponent } from './auth-page-structure/signin/signin.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AuthPageStructureComponent } from './auth-page-structure/auth-page-structure.component';
+import { SignupComponent } from './auth-page-structure/signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    NavbarComponent,
+    FooterComponent,
+    AuthPageStructureComponent,
+    SignupComponent,
+    HomeComponent,
+    AboutmeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
